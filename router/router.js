211f5f8f31085fd16f32cbe9c55614e099c4d57d
赵庆浩 <zhaoqinghao@shuidihuzhu.com>
2019-03-15 15:53:11 +0800

    修改代码

--- router/router.js
+++ router/router.js
@@ -1,7 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '../src/views/Home.vue'
-
 Vue.use(Router)
 
 export default new Router({
@@ -9,15 +7,7 @@ export default new Router({
     {
       path: '/',
       name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ '../src/views/About.vue')
+      component: () => import('../src/views/index.vue')
     }
   ]
 })
